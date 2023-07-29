55db18e080c00fd13db2a0e0f82259bab549aa7c
Dharamveer <dharammveer945734@outlook.com>
2023-07-29 15:55:34 +0530

    Some changes in Backend

--- Backend/db.js
+++ Backend/db.js
@@ -1,24 +1,10 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require("mongoose")
+require('dotenv').config()
 
-// Retrieve the MongoDB connection URL from the environment variable
-const dbURI = process.env.mongourl;
+const connection = mongoose.connect(process.env.mongoURL)
+.then(() => console.log('Connected to Talent-tech Server'))
+.catch((err) => console.error(err));
 
-// Function to connect to MongoDB
-const connection = async () => {
-  try {
-    const connection = await mongoose.connect(dbURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("Connected to MongoDB");
-    return connection;
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    throw error;
-  }
-};
-
-// Export the function to be used in other parts of the application
-module.exports = { connection };
+module.exports = {
+    connection
+}
