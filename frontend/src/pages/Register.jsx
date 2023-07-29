411c3c18bf7f2a64071a78e998e66754b1a8acbb
ank501 <ankitsyadav246@gmail.com>
2023-07-29 22:56:38 +0530

    pathname changed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
-import { signup } from '../redux/authreducer/action';
+import { signup } from '../redux/authReducer/action';
 
 
 const Register = () => {
