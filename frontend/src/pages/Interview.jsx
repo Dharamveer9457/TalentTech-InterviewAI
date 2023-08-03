5295eac7287a14a66c5e983a50d937dac7894f9e
ank501 <ankitsyadav246@gmail.com>
2023-08-03 12:28:20 +0530

    prompt is updated

--- frontend/src/pages/Interview.jsx
+++ frontend/src/pages/Interview.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/interview.css"
 import Chat from '../components/Chat'
 import { useDispatch } from 'react-redux'
@@ -12,17 +12,23 @@ const Interview = () => {
   const token = localStorage.getItem('ch-token') || '';
   const dispatch = useDispatch()
   // const [userPromt,setPrompt] = useState("");
-  const [level,setLevel] = useState("");
+  const [level,setLevel] = useState("basic");
   const [category,setcategory] = useState("");
-  // const [track ,setTrack] = useState("node");
-  const [prompt ,setPrompt] =useState(`You are an interviewer. Ask me 3 technical questions related to ${category} of ${level} level , one after the other. You should go to the next question only after I give an answer to the already asked question.Give me feedback at the end and give me rating out of 10. Feedback will gonna be calculate on three parameters that is communication skills, technical knowledge and problem solving.`)
+  const [track ,setTrack] = useState( "node");
+  const [prompt ,setPrompt] =useState("")
   // 
+
+ useEffect(()=>{
+  setPrompt(`You are an interviewer. Ask me 3 technical questions of  ${level} level related to ${track} stack, one by one only after getting response for previous question. You should go to the next question only after I give an answer to the already asked question.Give me feedback at the end and give me rating out of 10. Feedback will gonna be calculate on three parameters that is communication skills, technical knowledge and problem solving.`)
+ },[level ,track])
+
+
   const startInterview = () => {
     const data = {
-     track:category,
+     track,
      prompt
     }
-    console.log(data);
+    console.log(data.prompt);
     axios.post(`${baseUrl}/chatPrompt`,data)
     .then(res=>
     alert(res.data.res))
@@ -32,13 +38,13 @@ const Interview = () => {
   
   const finishInterview = () => {
     const finishPrompt = {
-      track:category,
+      track,
       prompt:"Finish The interview and give the Feedback on the basis of communication skill , technical skill ,problem solving , and critical thinking "
      }
     axios.post(`${baseUrl}/chatPrompt`,finishPrompt)
     .then(res=> alert(res.data.res))
     .catch(err=>console.log(err))
-    console.log(level,category)
+    console.log(level,track)
   }
 
   const handleLogout = ()=> {
@@ -52,17 +58,17 @@ const Interview = () => {
        {/* <h2>TalentTech Interview</h2> */}
       <div className='top'>
         <div>
-         <select onChange={(e)=>setcategory(e.target.value)}>
+         <select value={track} onChange={(e)=>setTrack(e.target.value)}>
           <option value="">Selet Category</option>
-          <option value="mern">MERN</option>
+          <option value="MERN">MERN</option>
           <option value="node">Node</option>
           <option value="java">Java</option>
          </select>
-         <select onChange={(e)=>setLevel(e.target.value)}>
+         <select value={level} onChange={(e)=>setLevel(e.target.value)}>
           <option value="">Selet Level</option>
-          <option value="beginner">Beginner</option>
-          <option value="intermediate">Intermediate</option>
-          <option value="advance">Advance</option>
+          <option value="basic">Beginner</option>
+          <option value="medium">Intermediate</option>
+          <option value="hard">Advance</option>
          </select>
          </div>
          <div>
